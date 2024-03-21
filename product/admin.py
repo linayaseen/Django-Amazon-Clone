96a197845dad09d546f74b3bf2fdbdc5fff96e3f
linayaseen <linayaseen@outlook.com>
2024-03-21 11:23:35 +0200

    add models to admin, add tabular inline

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from.models import Product,Brand,Review, ProductImages
+
+class ProductImagesInline(admin.TabularInline):
+    model= ProductImages
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines=[ProductImagesInline]
+ 
+admin.site.register(Product)
+admin.site.register(ProductImages)
+admin.site.register(Brand)
+admin.site.register(Review)
