587375013470838d90812393b0a4685a774d05d4
linayaseen <linayaseen@outlook.com>
2024-03-23 13:33:26 +0200

    show product reviews from view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
 
@@ -7,6 +8,18 @@ from .models import Product,Brand,Review
 class ProductList(ListView):
     model=Product
     
+#context{},queryset: product.object.all(): 1:option 2:method :override
+#queryset: main data :datail product 
+#context: extra data :reviews, images
+
+#queryset:[products] :filter
+#context: user
     
-class ProductDetail(DetailView):
+class ProductDetail(DetailView): 
     model=Product
+    
+    def get_context_data(self, **kwargs: Any) :
+        context = super().get_context_data(**kwargs)
+        context["reviews"]= Review.objects.filter(product=self.get_object())
+        return context
+    
